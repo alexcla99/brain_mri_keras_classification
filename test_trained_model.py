c8a5d8d90b0b62714025473d629d841efbc9c9d4
alexcla99 <80273148+alexcla99@users.noreply.github.com>
2022-07-12 09:18:09 +0200

    Update test_trained_model.py

--- test_trained_model.py
+++ test_trained_model.py
@@ -29,7 +29,12 @@ if __name__ == "__main__":
             tf_configure()
             # Load the test dataset
             info("Loading the test dataset")
-            train_dataset, val_dataset, test_datast = load_dataset(train_data_dir)
+            train_dataset, val_dataset, test_datast = load_dataset(
+                train_data_dir,
+                augment=False,
+                norm_type="threshold",
+                img_size=img_size
+            )
             info("Using test samples" % len([_ for _ in test_dataset]))
             # Load the selected model
             info("Loading the selected model (%s)" % model_name)
