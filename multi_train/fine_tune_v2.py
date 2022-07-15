442e4070e393d3245bc1b39de4a404df32804667
alexcla99 <80273148+alexcla99@users.noreply.github.com>
2022-07-15 15:16:08 +0200

    Update fine_tune_v2.py

--- multi_train/fine_tune_v2.py
+++ multi_train/fine_tune_v2.py
@@ -10,8 +10,6 @@ def run_fine_tune(model_name:str, settings:dict, res_folder:str):
     try:
         # Starting a fresh session
         tf.print("##### FINE TUNING PHASE #####", output_stream=sys.stdout)
-        tf.keras.backend.clear_session()
-        tf_configure()
         # Load params
         tf.print("Loading parameters", output_stream=sys.stdout)
         params = load_params()
@@ -43,7 +41,7 @@ def run_fine_tune(model_name:str, settings:dict, res_folder:str):
         # with mirrored_strategy.scope():
         model = get_model(img_size[0], img_size[1], img_size[-1])
         model.load_weights(os.path.join(results_dir, "%s.h5" % res_folder))
-        tf.print(model.summary(), output_stream=sys.stdout)
+        model.summary()
         # Freeze the model except its classifier (the four last layers)
         tf.print("Freezing the selected model except its classifier", output_stream=sys.stdout)
         if method == "tl":
@@ -55,7 +53,7 @@ def run_fine_tune(model_name:str, settings:dict, res_folder:str):
         else:
             raise Exception("Unknow method (not tl. nor ft.!)")
         for i in range(len(model.layers)):
-            tf.print("Trainable layer %d: %s" % (i, model.layers[i].trainable), output_stream=sys.stdout)
+            tf.print("Trainable layer %d: %s" %(i, str(model.layers[i].trainable)), output_stream=sys.stderr)
         # Compile the model
         tf.print("Compiling the model", output_stream=sys.stdout)
         model.compile(
@@ -76,7 +74,7 @@ def run_fine_tune(model_name:str, settings:dict, res_folder:str):
         )
         # Train the model
         tf.print("Transfer learning the model", output_stream=sys.stdout)
-        model.fit(
+        history = model.fit(
             train_dataset,
             validation_data=val_dataset,
             epochs=params["epochs"],
@@ -95,7 +93,7 @@ def run_fine_tune(model_name:str, settings:dict, res_folder:str):
         )
         # Save model's history
         tf.print("Saving model's history", output_stream=sys.stdout)
-        history = {
+        full_history = {
             params["metrics"][0]: [],
             "val_%s" % params["metrics"][0]: [],
             "loss": [],
@@ -103,10 +101,10 @@ def run_fine_tune(model_name:str, settings:dict, res_folder:str):
             "evaluation": evaluation
         }
         for i, metric in enumerate([params["metrics"][0], "loss"]):
-            history[metric].append(model.history.history[metric])
-            history["val_%s" % metric].append(model.history.history["val_%s" % metric])
+            full_history[metric].append(history.history[metric])
+            full_history["val_%s" % metric].append(history.history["val_%s" % metric])
         with open(os.path.join(results_dir, "%s_ft_metrics.json" % res_folder), "w+") as handle:
-            handle.write(json.dumps(history))
+            handle.write(json.dumps(full_history))
             handle.close()
         # End of the program
         tf.print("Fine tuning done", output_stream=sys.stdout)
